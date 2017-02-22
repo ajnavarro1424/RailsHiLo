f37d7a930377bad5b18d60cb3f09b0e886c88f75
cabelhigh <cabelhigh@yahoo.com>
2017-02-22 15:00:22 -0800

    Form Functionality Added

--- battleship/app/controllers/game_controller.rb
+++ battleship/app/controllers/game_controller.rb
@@ -3,31 +3,44 @@ class GameController < ApplicationController
   def try
     session[:guess_counter] = Array.new if session[:guess_counter].nil?
     @guess_counter = session[:guess_counter]
+    if @guess_counter.length>6
+      flash.now[:alert] = %Q[You've run out of guesses! Please restart the game.
+        <form  action="new_game" method="get">
+          <input type="submit" v? alue="Reset">
+        </form>
+      ]
 
-    if @rand == nil
+    end
+
+    @rand = session[:rand]
+    if @rand.blank?
+      puts "-----New Random Number!!!--------"
       r = Random.new
-      session[:rand] = r.rand(1..100) if session[:rand].nil?
+      session[:rand] = r.rand(1..100)
       @rand = session[:rand]
     end
 
-    @player = params[:player]
-    if params[:player] == nil
+
+    session[:username] = params[:username] if session[:username] != params[:username] && !params[:username].blank?
+    @player = session[:username]
+
+    if @player.blank?
       @player = "No username entered"
     end
 
     @guess = params[:guess]
-    if @guess != nil
+    if @guess != nil && @guess_counter.length<7
       # Deicison tree to evaluate @guess to @rand
       if @guess.to_i == @rand.to_i
         @response = "You guessed right!"
         reset
       elsif @guess.to_i < @rand.to_i
         @response = "You're too low!"
-
+        session[:guess_counter] << @guess
       else
         @response = "You're too high!"
+        session[:guess_counter] << @guess
       end
-      session[:guess_counter] << params[:guess]
     end
 
   end
@@ -36,7 +49,9 @@ class GameController < ApplicationController
     @guess = nil
     @response = ""
     @rand = nil
-    session[:guess_counter] = nil
+    session[:guess_counter] = []
+    session[:username] = nil
+    session[:rand] = nil
     redirect_to "/game"
   end
 end
